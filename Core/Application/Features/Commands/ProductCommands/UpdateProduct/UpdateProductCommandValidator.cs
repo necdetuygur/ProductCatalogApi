a4d91d03424f6298a8e4ddf2b51ea42072e28909
Necdet UYGUR <necdet.uygur@gmail.com>
2022-06-07 22:54:51 +0300

    Ürün satıldığında satıldı'sı true olmuyordu validasyonlar kapatıldı.

--- Core/Application/Features/Commands/ProductCommands/UpdateProduct/UpdateProductCommandValidator.cs
+++ Core/Application/Features/Commands/ProductCommands/UpdateProduct/UpdateProductCommandValidator.cs
@@ -13,17 +13,16 @@ namespace Application.Features.Commands.ProductCommands.UpdateProduct
         public UpdateProductCommandValidator()
         {
             RuleFor(x => x.Id).NotEmpty().WithMessage("Id is required");
-            RuleFor(x => x.Name).NotEmpty().WithMessage("Name is required");
-            RuleFor(x => x.Price).NotEmpty().WithMessage("Price is required");
-            RuleFor(x => x.Picture).NotEmpty().WithMessage("Picture is required");
-            RuleFor(x => x.Description).NotEmpty().WithMessage("Description is required");
-            RuleFor(x => x.CategoryId).NotEmpty().WithMessage("CategoryId is required");
-            RuleFor(x => x.BrandId).NotEmpty().WithMessage("BrandId is required");
-            RuleFor(x => x.ColorId).NotEmpty().WithMessage("ColorId is required");
-            RuleFor(x => x.UseCaseId).NotEmpty().WithMessage("UseCaseId is required");
-            RuleFor(x => x.IsOfferable).NotEmpty().WithMessage("IsOfferable is required");
-            RuleFor(x => x.UserId).NotEmpty().WithMessage("UserId is required");
-            
+            //RuleFor(x => x.Name).NotEmpty().WithMessage("Name is required");
+            //RuleFor(x => x.Price).NotEmpty().WithMessage("Price is required");
+            //RuleFor(x => x.Picture).NotEmpty().WithMessage("Picture is required");
+            //RuleFor(x => x.Description).NotEmpty().WithMessage("Description is required");
+            //RuleFor(x => x.CategoryId).NotEmpty().WithMessage("CategoryId is required");
+            //RuleFor(x => x.BrandId).NotEmpty().WithMessage("BrandId is required");
+            //RuleFor(x => x.ColorId).NotEmpty().WithMessage("ColorId is required");
+            //RuleFor(x => x.UseCaseId).NotEmpty().WithMessage("UseCaseId is required");
+            //RuleFor(x => x.IsOfferable).NotEmpty().WithMessage("IsOfferable is required");
+            //RuleFor(x => x.UserId).NotEmpty().WithMessage("UserId is required");
         }
     }
 }
