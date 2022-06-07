e9002214419580fad79abba96e79ec02dbd39be4
Necdet UYGUR <necdet.uygur@gmail.com>
2022-06-07 22:43:34 +0300

    Order update yapılabilmesi için id hariç tüm alanlar zorunluluktan kaldırıldı.

--- Core/Application/Features/Commands/OrderCommands/UpdateOrder/UpdateOrderCommandValidator.cs
+++ Core/Application/Features/Commands/OrderCommands/UpdateOrder/UpdateOrderCommandValidator.cs
@@ -13,10 +13,10 @@ namespace Application.Features.Commands.OrderCommands.UpdateOrder
         public UpdateOrderCommandValidator()
         {
             RuleFor(x => x.Id).NotEmpty().WithMessage("Id is required");
-            RuleFor(x => x.Price).NotEmpty().WithMessage("Price is required");
-            RuleFor(x => x.ProductId).NotEmpty().WithMessage("ProductId is required");
-            RuleFor(x => x.UserId).NotEmpty().WithMessage("UserId is required");
-            RuleFor(x => x.StatusId).NotEmpty().WithMessage("StatusId is required");
+            //RuleFor(x => x.Price).NotEmpty().WithMessage("Price is required");
+            //RuleFor(x => x.ProductId).NotEmpty().WithMessage("ProductId is required");
+            //RuleFor(x => x.UserId).NotEmpty().WithMessage("UserId is required");
+            //RuleFor(x => x.StatusId).NotEmpty().WithMessage("StatusId is required");
         }
     }
 }
